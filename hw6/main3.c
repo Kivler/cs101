081410d2ff7c9e87ab79ca950e929eebf157ad09
Kivler <113400157+Kivler@users.noreply.github.com>
2022-12-28 10:54:34 +0800

    Update main3.c

--- hw6/main3.c
+++ hw6/main3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(){
+    int year = 2022;
+    
+    if (year%400==0){
+        printf("是閏年");
+    }
+    else if(year %4==0 && year%100!=0){
+        printf("是閏年");
+    }
+    else {
+        printf("不是閏年");
+    }
+    return 0;
+}
