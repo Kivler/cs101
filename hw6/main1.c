c482cbf7bf47215cfecaa0c7e944efcc0ed40140
Kivler <113400157+Kivler@users.noreply.github.com>
2022-12-28 10:17:28 +0800

    Update main1.c

--- hw6/main1.c
+++ hw6/main1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    int i=10;
+    
+    if(i%3==0 && i%5!=0){
+        printf("Love");
+    }
+    else if(i%5==0 && i%3!=0){
+        printf("IU");
+    }
+    else if(i%3==0 && i%5==0){
+        printf("Love IU");
+    }
+    else{
+        printf("%d ",i);
+    }
+    return 0;
+}
