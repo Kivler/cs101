2ef77f0c1139123986d664d613a4e271d0478b84
Kivler <113400157+Kivler@users.noreply.github.com>
2022-12-29 17:04:34 +0800

    Update main.c

--- qz2/main.c
+++ qz2/main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
-/* global variable declaration */
 int rows = 5;
-void print_spaces(int r) {
-    /* local variable declaration */
-    int i;
-}
-
-void print_stars(int r) {
-    /* local variable declaration */
-    int i;
-}
-
-int main() {
-    /* local variable declaration */
-    int j;
-    for (int i = rows; i >= 1; --i) {
-        print_spaces(i);
-        print_stars(i);
+void printSpaces(int r,int rows){
+    for(int i=rows; i>r; i--){
+        printf(" ");
     }
-     
+}
+
+void printStars(int r){
+    int i;
+    for(int i=1; i<=r; i++){
+        printf("* ");
+    }
+}
+
+int main()
+{
+    int j = rows;
+    for(int i = rows; i >= 1; --i){
+        printSpaces(i,j);
+        printStars(i);
+        printf("\n");
+    }
+
     return 0;
 }
