d181d196bf20c758aae5e5793a805dd5196a4d22
Kivler <113400157+Kivler@users.noreply.github.com>
2022-12-29 13:38:47 +0800

    Update main1.c

--- hw7/main1.c
+++ hw7/main1.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-int main(){
-    int n=7;
-    int spc = n;
-    
-    for(int i=1;i<=n;i++){
-        for(int k=spc;k>=1;k--){
-            printf(" ");
-        }
-        for (int j=1;j<=i;j++){
+
+int main()
+{
+    int n=7,i=0,j=0;
+    char spc=' ';
+    for(i=1;i<=n;i++){
+        printf("%*c",n-i+1,' ');
+        for(j=1;j<=i;j++){
             printf("%d ",i);
         }
         printf("\n");
-        spc--;
     }
-      
     return 0;
 }
